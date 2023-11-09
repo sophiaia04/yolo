afc1160075be1fb0735630b3597bcdd2026e90d9
sophiaia04 <148435787+sophiaia04@users.noreply.github.com>
2023-11-09 16:16:23 +0100

    Update age_gender.py

--- age_gender.py
+++ age_gender.py
@@ -44,7 +44,7 @@ if st.button("Age and Gender Estimation", use_container_width=True):
     st.title("Webcam Live Feed")
     run = True
     FRAME_WINDOW = st.image([])
-    camera = cv2.VideoCapture(0)
+    camera = cv2.VideoCapture(1)
 
     # Check if the webcam is working
     if not camera.isOpened():
