5c170ef93607f01b3a9aaa124809d9edd31384e0
sophiaia04 <sophiatran404@gmail.com>
2023-11-16 11:19:13 +0100

    Initial commit

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
 import streamlit as st
-st.write('halalp')
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration
+
+# Define RTC configuration for ICE servers (STUN/TURN)
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+def main():
+    st.title("WebRTC Test")
+
+    # Create a webrtc streamer
+    webrtc_streamer(key="example", rtc_configuration=RTC_CONFIGURATION)
+
+if __name__ == "__main__":
+    main()
